eb70fc6407518c44afc17d293b101e6fb99b22a1
Shaun Lee <shaundlee.0103@gmail.com>
2020-09-11 11:55:23 -0400

    Mod Ring Buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,30 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = [] 
+        self.size = 0
+        self.tail = -1
+        self.head = 0
 
     def append(self, item):
-        pass
+        #check if storage is full
+        if self.size == self.capacity:
+            #if true, enqueue item to tail, and decrease the size
+            self.enqueue(item)
+            self.size -= 1
+        else:
+            #otherwise, enqueue item    
+            self.storage.append(item)
+            self.size += 1
 
     def get(self):
-        pass
+        return self.storage
+
+    def enqueue(self, item):
+        #shift tail to next position in loop
+        #append item to new tail location
+        #increment buffer size
+        self.tail = (self.tail + 1) % self.capacity
+        self.storage[self.tail] = item
+        self.size += 1
+
