f27cabc975f896bdc5f9508877c068a086a30acc
Shaun Lee <shaundlee.0103@gmail.com>
2020-09-11 13:48:50 -0400

    Mod names and reverse

--- names/names.py
+++ names/names.py
@@ -10,14 +10,41 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []  # Return the list of duplicates in this data structure
+# duplicates = []  # Return the list of duplicates in this data structure
+duplicates = [ name for name in names_1 if name in names_2 ]  # Return the list of duplicates in this data structure // 0.4 seconds slower than using stack
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
+'''class Stack:
+    def __init__(self):
+        self.size = 0
+        self.storage = []
+
+    def __len__(self):
+        return self.size
+
+    def push(self, value):
+        self.size += 1
+        self.storage.append(value)
+
+    def pop(self):
+        if self.size == 0:
+            return None
+        self.size -= 1
+        return self.storage.pop()
+
+search = Stack()
+for name in names_1:
+    search.push(name)
+
+for item in search.storage:
+    if item in names_2:
+        duplicates.append(item)
+'''
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
